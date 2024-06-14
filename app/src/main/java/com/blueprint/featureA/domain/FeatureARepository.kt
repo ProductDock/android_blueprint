1e21353c7f58b60b4dfbd0ec157d1b48f4a429b7
Bojana Armacki Dragutinovic <dragutinovic.bojana@gmail.com>
2024-06-14 10:26:03 +0200

    refactor(data): feature repository to expose flow

--- app/src/main/java/com/blueprint/featureA/domain/FeatureARepository.kt
+++ app/src/main/java/com/blueprint/featureA/domain/FeatureARepository.kt
@@ -1,8 +1,8 @@
 package com.blueprint.featureA.domain
 
 import com.blueprint.featureA.domain.model.FeatureA
+import kotlinx.coroutines.flow.Flow
 
 interface FeatureARepository {
-
-    suspend fun getFeaturesA() : Result<List<FeatureA>>
+    fun getFeaturesA() : Flow<Result<List<FeatureA>>>
 }
--- app/src/main/java/com/blueprint/featureA/ui/FeatureAViewModel.kt
+++ app/src/main/java/com/blueprint/featureA/ui/FeatureAViewModel.kt
@@ -7,6 +7,7 @@ import com.blueprint.featureA.domain.model.FeatureA
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -18,10 +19,15 @@ class FeatureAViewModel @Inject constructor(
     private val _features = MutableStateFlow(emptyList<FeatureA>())
     val features = _features.asStateFlow()
 
+    // TODO Don't trigger explicitly from UI
     fun getFeaturesA() {
         viewModelScope.launch {
-            _features.value = featureARepository.getFeaturesA().getOrElse { emptyList() }
+            featureARepository.getFeaturesA().map { result ->
+                result.fold(
+                    onSuccess = { it } ,
+                    onFailure = { emptyList() },
+                )
+            }
         }
     }
-
 }
