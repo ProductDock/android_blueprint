4d579bdf1df3c5cdc8db1e47affb29347cc6da41
Ivan Nenadovic <ivan.nenadovic@productdock.com>
2024-06-24 09:29:37 +0200

    refactor: convered to expression body

--- app/src/main/java/com/blueprint/featureA/mapper/FeatureMapper.kt
+++ app/src/main/java/com/blueprint/featureA/mapper/FeatureMapper.kt
@@ -4,17 +4,13 @@ import com.blueprint.featureA.data.model.local.FeatureAEntity
 import com.blueprint.featureA.domain.model.FeatureA
 
 
-fun FeatureA.toEntity(): FeatureAEntity {
-  return FeatureAEntity(
-    string = this.string
-  )
-}
+fun FeatureA.toEntity(): FeatureAEntity = FeatureAEntity(
+  string = this.string
+)
 
-fun FeatureAEntity.toDomain(): FeatureA {
-  return FeatureA(
-    string = this.string
-  )
-}
+fun FeatureAEntity.toDomain(): FeatureA = FeatureA(
+  string = this.string
+)
 
 fun List<FeatureA>.toEntity(): List<FeatureAEntity> = map { it.toEntity() }
 
